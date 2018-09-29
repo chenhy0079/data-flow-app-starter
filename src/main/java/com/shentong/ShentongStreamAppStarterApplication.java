1fc44ebfbd1713a10e20677e76e7546a93643875
chenhy <chy9700@163.com>
2018-09-29 18:23:46 +0800

    parent project

---
+++ src/main/java/com/shentong/ShentongStreamAppStarterApplication.java
@@ -0,0 +1,12 @@
+package com.shentong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShentongStreamAppStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShentongStreamAppStarterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/shentong/ShentongStreamAppStarterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shentong;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShentongStreamAppStarterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
